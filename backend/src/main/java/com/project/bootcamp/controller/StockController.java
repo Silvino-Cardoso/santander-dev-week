61e91404a7361ceedf39390ea3c285d770349890
Silvino-Cardoso <silvino.itp@gmail.com>
2021-05-29 13:34:12 -0300

    Adicionado camada Controller e Dto da aplicação

---
+++ backend/src/main/java/com/project/bootcamp/controller/StockController.java
@@ -0,0 +1,64 @@
+package com.project.bootcamp.controller;
+
+import com.project.bootcamp.model.dto.StockDto;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/stock")
+public class StockController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDto> save(@RequestBody StockDto dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDto> update(@RequestBody StockDto dto){
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<StockDto>> findAll(){
+        List<StockDto> list = new ArrayList<>();
+        StockDto dto = new StockDto();
+        dto.setId(1L);
+        dto.setName("Magazine Luiza");
+        dto.setPrice(100.90);
+        dto.setVariation(100D);
+        dto.setDate(LocalDate.now());
+        list.add(dto);
+
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<StockDto> findById(@PathVariable Long id){
+        List<StockDto> list = new ArrayList<>();
+        StockDto stock1 = new StockDto();
+        stock1.setId(1L);
+        stock1.setName("Magazine Luiza");
+        stock1.setPrice(100.90);
+        stock1.setVariation(100D);
+        stock1.setDate(LocalDate.now());
+
+        StockDto stock2 = new StockDto();
+        stock2.setId(2L);
+        stock2.setName("Weg");
+        stock2.setPrice(78.10);
+        stock2.setVariation(100D);
+        stock2.setDate(LocalDate.now());
+
+        list.add(stock1);
+        list.add(stock2);
+
+        StockDto dtoSelect = list.stream().filter(x -> x.getId().compareTo(id) == 0).findFirst().get();
+
+        return ResponseEntity.ok(dtoSelect);
+    }
+}
